253c68934827fee608a80e07b9c58d28dbedef26
Camilo Polymeris <camilo@circleci.com>
2019-10-02 11:44:54 -0400

    Move test YAML to resources

--- src/test/java/com/circleci/connector/gitlab/singleorg/client/GitLabTest.java
+++ src/test/java/com/circleci/connector/gitlab/singleorg/client/GitLabTest.java
@@ -8,7 +8,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import io.dropwizard.util.Resources;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.Optional;
 import org.gitlab4j.api.GitLabApi;
 import org.gitlab4j.api.GitLabApiException;
@@ -22,19 +25,19 @@ class GitLabTest {
   private static final ObjectMapper YAML_MAPPER = new ObjectMapper(new YAMLFactory());
   private static final int PROJECT_ID = 22;
   private static final String REF = "1234";
-  private static final String VALID_CONFIG_CONTENTS =
-      "version: 2.1\n"
-          + "jobs:\n"
-          + "  build:\n"
-          + "    docker: \n"
-          + "      - image: circleci/node:4.8.2 # the primary container, where your job's commands are run\n"
-          + "    steps:\n"
-          + "      - checkout # check out the code in the project directory\n"
-          + "      - run: echo \"hello world\" # run the `echo` command";
 
   private GitLabApi mockGitLabApi = Mockito.mock(GitLabApi.class);
   private RepositoryFileApi mockRepositoryFileApi = Mockito.mock(RepositoryFileApi.class);
   private GitLab gitLab = new GitLab(mockGitLabApi);
+
+  private static String readCircleCIConfigAsString(String filename) throws IOException {
+    URL resource = Resources.getResource(String.format("circleci-config/%s", filename));
+    return Resources.toString(resource, Charset.defaultCharset());
+  }
+
+  private static ObjectNode readCircleCIConfigAsObjectNode(String filename) throws IOException {
+    return (ObjectNode) YAML_MAPPER.readTree(readCircleCIConfigAsString(filename));
+  }
 
   @BeforeEach
   void setUp() {
@@ -43,179 +46,50 @@ class GitLabTest {
 
   @Test
   void extendEmptyCircleCiConfig() throws IOException {
-    ObjectNode expected =
-        (ObjectNode)
-            YAML_MAPPER.readTree(
-                "parameters:\n"
-                    + "  gitlab_git_uri:\n"
-                    + "    type: \"string\"\n"
-                    + "    default: \"\"\n"
-                    + "  gitlab_ssh_fingerprint:\n"
-                    + "    type: \"string\"\n"
-                    + "    default: \"\"\n"
-                    + "commands:\n"
-                    + "  git-checkout:\n"
-                    + "    steps:\n"
-                    + "    - add_ssh_keys:\n"
-                    + "        fingerprints:\n"
-                    + "        - \"<< pipeline.parameters.gitlab_ssh_fingerprint >>\"\n"
-                    + "    - run: \"mkdir -p ~/.ssh\\nssh-keyscan gitlab.com >> ~/.ssh/known_hosts\\ngit clone\\\n"
-                    + "        \\ --depth=1 << pipeline.parameters.gitlab_git_uri >> project\"\n");
+    ObjectNode expected = readCircleCIConfigAsObjectNode("empty.output.yaml");
     ObjectNode result = gitLab.extendCircleCiConfig(YAML_MAPPER.readTree(""));
     assertEquals(expected, result);
   }
 
   @Test
   void extendValidCircleCiConfigWithoutParametersOrCommands() throws IOException {
-    ObjectNode expected =
-        (ObjectNode)
-            YAML_MAPPER.readTree(
-                "version: 2.1\n"
-                    + "jobs:\n"
-                    + "  build:\n"
-                    + "    docker:\n"
-                    + "      - image: circleci/node:4.8.2\n"
-                    + "    steps:\n"
-                    + "      - git-checkout\n"
-                    + "      - run: echo hello\n"
-                    + "parameters:\n"
-                    + "  gitlab_git_uri:\n"
-                    + "    type: \"string\"\n"
-                    + "    default: \"\"\n"
-                    + "  gitlab_ssh_fingerprint:\n"
-                    + "    type: \"string\"\n"
-                    + "    default: \"\"\n"
-                    + "commands:\n"
-                    + "  git-checkout:\n"
-                    + "    steps:\n"
-                    + "    - add_ssh_keys:\n"
-                    + "        fingerprints:\n"
-                    + "        - \"<< pipeline.parameters.gitlab_ssh_fingerprint >>\"\n"
-                    + "    - run: \"mkdir -p ~/.ssh\\nssh-keyscan gitlab.com >> ~/.ssh/known_hosts\\ngit clone\\\n"
-                    + "        \\ --depth=1 << pipeline.parameters.gitlab_git_uri >> project\"\n");
-    String config =
-        "version: 2.1\n"
-            + "jobs:\n"
-            + "  build:\n"
-            + "    docker:\n"
-            + "      - image: circleci/node:4.8.2\n"
-            + "    steps:\n"
-            + "      - git-checkout\n"
-            + "      - run: echo hello";
+    ObjectNode expected = readCircleCIConfigAsObjectNode("valid-simple.output.yaml");
+    String config = readCircleCIConfigAsString("valid-simple.input.yaml");
     ObjectNode result = gitLab.extendCircleCiConfig(YAML_MAPPER.readTree(config));
     assertEquals(expected, result);
   }
 
   @Test
   void extendValidCircleCiConfigWithParameters() throws IOException {
-    ObjectNode expected =
-        (ObjectNode)
-            YAML_MAPPER.readTree(
-                "version: 2.1\n"
-                    + "jobs:\n"
-                    + "  build:\n"
-                    + "    docker:\n"
-                    + "      - image: circleci/node:4.8.2\n"
-                    + "    steps:\n"
-                    + "      - git-checkout\n"
-                    + "      - run: echo hello\n"
-                    + "parameters:\n"
-                    + "  foo:\n"
-                    + "    type: boolean\n"
-                    + "    default: false\n"
-                    + "  gitlab_git_uri:\n"
-                    + "    type: \"string\"\n"
-                    + "    default: \"\"\n"
-                    + "  gitlab_ssh_fingerprint:\n"
-                    + "    type: \"string\"\n"
-                    + "    default: \"\"\n"
-                    + "commands:\n"
-                    + "  git-checkout:\n"
-                    + "    steps:\n"
-                    + "    - add_ssh_keys:\n"
-                    + "        fingerprints:\n"
-                    + "        - \"<< pipeline.parameters.gitlab_ssh_fingerprint >>\"\n"
-                    + "    - run: \"mkdir -p ~/.ssh\\nssh-keyscan gitlab.com >> ~/.ssh/known_hosts\\ngit clone\\\n"
-                    + "        \\ --depth=1 << pipeline.parameters.gitlab_git_uri >> project\"\n");
-    String config =
-        "version: 2.1\n"
-            + "jobs:\n"
-            + "  build:\n"
-            + "    docker:\n"
-            + "      - image: circleci/node:4.8.2\n"
-            + "    steps:\n"
-            + "      - git-checkout\n"
-            + "      - run: echo hello\n"
-            + "parameters:\n"
-            + "  foo:\n"
-            + "    type: boolean\n"
-            + "    default: false";
+    ObjectNode expected = readCircleCIConfigAsObjectNode("valid-parameters.output.yaml");
+    String config = readCircleCIConfigAsString("valid-parameters.input.yaml");
     ObjectNode result = gitLab.extendCircleCiConfig(YAML_MAPPER.readTree(config));
     assertEquals(expected, result);
   }
 
   @Test
   void extendValidCircleCiConfigWithCommand() throws IOException {
-    ObjectNode expected =
-        (ObjectNode)
-            YAML_MAPPER.readTree(
-                "version: 2.1\n"
-                    + "jobs:\n"
-                    + "  build:\n"
-                    + "    docker:\n"
-                    + "      - image: circleci/node:4.8.2\n"
-                    + "    steps:\n"
-                    + "      - git-checkout\n"
-                    + "      - run: echo hello\n"
-                    + "parameters:\n"
-                    + "  gitlab_git_uri:\n"
-                    + "    type: \"string\"\n"
-                    + "    default: \"\"\n"
-                    + "  gitlab_ssh_fingerprint:\n"
-                    + "    type: \"string\"\n"
-                    + "    default: \"\"\n"
-                    + "commands:\n"
-                    + "  sayhi:\n"
-                    + "    steps:\n"
-                    + "    - run: echo hi\n"
-                    + "  git-checkout:\n"
-                    + "    steps:\n"
-                    + "    - add_ssh_keys:\n"
-                    + "        fingerprints:\n"
-                    + "        - \"<< pipeline.parameters.gitlab_ssh_fingerprint >>\"\n"
-                    + "    - run: \"mkdir -p ~/.ssh\\nssh-keyscan gitlab.com >> ~/.ssh/known_hosts\\ngit clone\\\n"
-                    + "        \\ --depth=1 << pipeline.parameters.gitlab_git_uri >> project\"\n");
-    String config =
-        "version: 2.1\n"
-            + "jobs:\n"
-            + "  build:\n"
-            + "    docker:\n"
-            + "      - image: circleci/node:4.8.2\n"
-            + "    steps:\n"
-            + "      - git-checkout\n"
-            + "      - run: echo hello\n"
-            + "commands:\n"
-            + "  sayhi:\n"
-            + "    steps:\n"
-            + "    - run: echo hi\n";
+    ObjectNode expected = readCircleCIConfigAsObjectNode("valid-commands.output.yaml");
+    String config = readCircleCIConfigAsString("valid-commands.input.yaml");
     ObjectNode result = gitLab.extendCircleCiConfig(YAML_MAPPER.readTree(config));
     assertEquals(expected, result);
   }
 
   @Test
   void extendInvalidCircleCiConfigFails() throws IOException {
-    String config = "not valid config";
+    String config = readCircleCIConfigAsString("invalid.input.yaml");
     ObjectNode result = gitLab.extendCircleCiConfig(YAML_MAPPER.readTree(config));
     assertNull(result);
   }
 
   @Test
-  void fetchCircleCiConfigSuccessfully() throws GitLabApiException {
+  void fetchCircleCiConfigSuccessfully() throws GitLabApiException, IOException {
     RepositoryFile mockRepositoryFile = Mockito.mock(RepositoryFile.class);
 
     Mockito.when(mockRepositoryFileApi.getFile(PROJECT_ID, GitLab.CIRCLECI_CONFIG_PATH, REF))
         .thenReturn(mockRepositoryFile);
-    Mockito.when(mockRepositoryFile.getDecodedContentAsString()).thenReturn(VALID_CONFIG_CONTENTS);
+    Mockito.when(mockRepositoryFile.getDecodedContentAsString())
+        .thenReturn(readCircleCIConfigAsString("valid-simple.input.yaml"));
 
     Optional<String> result = gitLab.fetchCircleCiConfig(PROJECT_ID, REF);
     assertFalse(result.isEmpty());
@@ -231,12 +105,13 @@ class GitLabTest {
   }
 
   @Test
-  void fetchInvalidYamlError() throws GitLabApiException {
+  void fetchInvalidYamlError() throws GitLabApiException, IOException {
     RepositoryFile mockRepositoryFile = Mockito.mock(RepositoryFile.class);
 
     Mockito.when(mockRepositoryFileApi.getFile(PROJECT_ID, GitLab.CIRCLECI_CONFIG_PATH, REF))
         .thenReturn(mockRepositoryFile);
-    Mockito.when(mockRepositoryFile.getDecodedContentAsString()).thenReturn("{{{");
+    Mockito.when(mockRepositoryFile.getDecodedContentAsString())
+        .thenReturn(readCircleCIConfigAsString("bad-yaml.input.yaml"));
 
     Optional<String> result = gitLab.fetchCircleCiConfig(PROJECT_ID, REF);
     assertTrue(result.isEmpty());
